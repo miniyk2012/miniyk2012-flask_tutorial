b7a7ef4326681f09ab2e7cdca5774eb496fd7b0c
Thomas Young <yk_ecust_2007@163.com>
2019-05-30 00:30:53 +0800

    [feature] 运行测试，生成测试报告

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # miniyk2012-flask_tutorial
 flask官网教程，我仔细学习并写了很多注释
+
+本地启动方法：pip install flask
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run
+
+
+测试用例运行方法：pip install pytest coverage
+在setup.cfg中指定了测试目录和源代码目录
+运行pytest
+运行获得用例详情：运行pytest -v
+运行用例并获得覆盖率：
+1. coverage run -m pytest
+2. coverage report
+3. [optional] coverage html, 覆盖统计会生成在htmlcov目录下
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -20,7 +20,7 @@ def get_post(id, check_author=True):
     if post is None:
         abort(404, "Post id {0} doesn't exist.".format(id))
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post['author_id'] != g.user['id']:  # 修改人/删除人必须是发帖人
         abort(403)
 
     return post
@@ -98,4 +98,4 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('index'))
