dd97101a81d83a99362da67dfb927f1e63f76cf1
Thomas Young <yk_ecust_2007@163.com>
2019-05-29 23:30:09 +0800

    [feature] test auth, 我仔细理解了测试结构中context的生命周期,并做了相当详细的注释, 再遇到问题可以仔细的复习。相信对今后的学习也有相当大的帮助

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -15,16 +15,38 @@ def test_register(client, app):
         ).fetchone() is not None
 
 
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+        ('', '', b'Username is required.'),
+        ('a', '', b'Password is required.'),
+        ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+        ('a', 'test', 'Incorrect username.'),
+        ('test', 'a', 'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.get_data(as_text=True)
+
+
 def test_login(client, auth):
+    assert auth._client is client  # autu._client is client, 相当于同一个客户端访问服务端, 因此session在同一个会话的多个请求间可以使用
     assert client.get('/auth/login').status_code == 200
     response = auth.login()
     assert response.headers['Location'] == 'http://localhost/'
-
     with client:
         """The test client can be used in a ``with`` block to defer the closing down(延迟清理)
         of the context until the end of the ``with`` block. """
         client.get('/')
-        assert session['user_id'] == 1  # 因为前面的auth.login()，因此这里才能成功
+        assert session['user_id'] == 1  # 因为前面的auth.login()，因此这里才能成功, 而且因为autu._client正是这里的client，session才保留
         assert g.user['username'] == 'test'
         a = g.db
         x = g.user
@@ -36,3 +58,19 @@ def test_login(client, auth):
         y = g.user
         assert a != b
         assert x is not y
+
+
+def test_logout(client, auth):
+    auth.login()
+    with client:
+        client.get('/')
+        assert 'user_id' in session
+        assert g.user is not None
+
+        auth.logout()
+        assert 'user_id' not in session
+        assert g.user is not None  # logout结束后, 代码逻辑可知g.user并没有被清空
+
+        client.get('/')
+        assert 'user_id' not in session
+        assert g.user is None  # 下次再登陆, 由于session不存在了, 在load_logged_in_user方法中g.user被赋值为None()
